c13c5748f0c506daf53cd17ac0713f836675f327
atanasivanov17 <ivanov.atanas.e25092@mcast.edu.mt>
2021-02-12 14:54:29 +0100

    Setup the server and created database models

---
+++ server/routes/product.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { getProducts, newProduct } = require("../controllers/productController");
+
+router.route("/product").get(getProducts);
+router.route("/product/new").post(newProduct);
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+//Setting up config file
+dotenv.config({ path: "server/config/config.env" });
+
+//Connecting to database
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
+});
